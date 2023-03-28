fcd8f5ad367a799db2529ee411b658b22f5c5898
shivam <084ssharan@gmail.com>
2023-03-28 09:17:02 +0530

    CHANGE

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(function (req, res, next) {
   //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE,UPDATE",
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization",
